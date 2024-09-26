3f5fd74d5674a517a018db3fc0722a7a369a1af5
Sanjay-5406 <ssysanjay5406@gmail.com>
2024-09-26 09:44:24 +0530

    Tailored intro

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Introductory_Project
+All the personal info in this file aren't og please mail to the gmail_id if u want to contact me
+😊
---
+++ proto.js
@@ -0,0 +1,5 @@
+function showAlert(){
+    alert("Welcome !");
+    
+}
+document.getElementById("alertButton").addEventListener("click",showAlert);
